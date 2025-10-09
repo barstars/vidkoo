51cb4b0d3d9c487b092c1baf369d5761bdc14c32
Tangirkul Muhammed <mtangirkul@gmail.com>
2025-10-09 22:57:02 +0500

    Create load static files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 # 5432
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.apis.router import router as apiRouter
-
 from app.static_controller.load_page import router as load_page
 
 
@@ -10,4 +10,5 @@ app = FastAPI()
 
 app.include_router(apiRouter, prefix="/api", tags=["api"])
 
+app.mount("/static", StaticFiles(directory="front"), name="static")
 app.include_router(load_page)
